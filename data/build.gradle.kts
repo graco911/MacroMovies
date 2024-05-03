bb35b31d6b1cf6a96f93242f0227b80a3cd601d7
CarlosGraniel <cgraniel@tiprotec.com>
2024-05-03 17:41:09 -0600

    :construction: Added Firebase Authentication.

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("kotlin-android")
     id("kotlin-parcelize")
     id("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -47,6 +48,7 @@ dependencies {
     implementation(libs.androidx.paging.runtime.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
